531acdcb64bcbffb59598270a9dbefbec3af8dfc
Bogdan <bogdan_kolesov@bk.ru>
2021-10-31 07:48:52 +0600

    fix: App

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import RootNavigation from "./navigation";
 
+//TODO: Error's, saveState, colorInditation, styles like styleSheet
 
 export default function App() {
   return <RootNavigation />
